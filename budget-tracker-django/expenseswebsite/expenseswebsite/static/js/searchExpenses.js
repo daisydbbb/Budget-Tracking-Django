12288fd192f90be4961fc757fa4d0a43eca0123a
“Daisy <daisyding666@gmail.com>
2024-01-19 10:57:10 -0800

    add income

---
+++ budget-tracker-django/expenseswebsite/expenseswebsite/static/js/searchExpenses.js
@@ -0,0 +1,47 @@
+const searchField = document.querySelector('#searchField')
+const tableEmpty = document.querySelector('.table-empty')
+const tableNotEmpty = document.querySelector('.table-not-empty')
+const pagination = document.querySelector('.pagination-container')
+const tbody = document.querySelector('.empty-table-body')
+
+
+tableEmpty.style.display = 'none';
+
+searchField.addEventListener('keyup', (e)=> {
+  const searchValue = e.target.value;
+
+  if (searchValue.trim().length > 0) {
+    pagination.style.display = 'none';
+    tbody.innerHTML = '';
+
+    fetch('/search-expenses', {  // Not /expenses/search-expenses
+      body: JSON.stringify({searchText: searchValue}),
+      method: "POST",
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      tableNotEmpty.style.display = 'none';
+      tableEmpty.style.display = 'block';
+      if (data.length === 0) {
+        tbody.innerHTML = 'No result found'
+      } else {
+        // add seached item to the empty table one by one
+        data.forEach(item => {
+          tbody.innerHTML+=`
+          <tr>
+            <td>${item.amount}</td>
+            <td>${item.category}</td>
+            <td>${item.description}</td>
+            <td>${item.date}</td>
+          </tr>
+          `
+        })
+      }
+    })
+  } else {
+    tableEmpty.style.display = 'none';
+    tableNotEmpty.style.display = 'block';
+    pagination.style.display = 'block';
+  }
+})
--- budget-tracker-django/expenseswebsite/incomes/views.py
+++ budget-tracker-django/expenseswebsite/incomes/views.py
@@ -1,45 +1,45 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Expense, Category
+from .models import Income, Category
 from django.contrib import messages
 from django.core.paginator import Paginator
 import json
 from django.http import JsonResponse
 from userpreferences.models import UserPreference
 
-def search_expense(request):
+def search_income(request):
   if request.method=="POST":
     search_str = json.loads(request.body).get('searchText')
 
-    expenses = Expense.objects.filter(
-            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
-            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
-            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+    incomes = Income.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Income.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Income.objects.filter(
+            description__icontains=search_str, owner=request.user) | Income.objects.filter(
             category__icontains=search_str, owner=request.user)
-    data = expenses.values()
+    data = incomes.values()
     return JsonResponse(list(data), safe=False)
 
 
 @login_required(login_url='/authentication/login/') # not working properly??
 def index(request):
   categories = Category.objects.all()
-  expenses = Expense.objects.filter(owner=request.user)
+  incomes = Income.objects.filter(owner=request.user)
   # add pagination
-  paginator = Paginator(expenses, 5)
+  paginator = Paginator(incomes, 5)
   page_number = request.GET.get('page')
   page_obj = Paginator.get_page(paginator, page_number)
   currency = UserPreference.objects.get(user=request.user).currency
   currency_abbre = currency.split('-')[0]
 
   context = {
-    'expenses': expenses,
+    'incomes': incomes,
     'page_obj': page_obj,
     'currency': currency_abbre,
   }
-  return render(request, 'expenses/index.html', context)
+  return render(request, 'incomes/index.html', context)
 
 
-def add_expense(request):
+def add_income(request):
   categories = Category.objects.all()
   context= {
     'categories': categories,
@@ -47,7 +47,7 @@ def add_expense(request):
   }
 
   if request.method == 'GET':
-    return render(request, 'expenses/add_expense.html', context)
+    return render(request, 'incomes/add_income.html', context)
 
   if request.method == 'POST':
     amount = request.POST['amount']
@@ -57,28 +57,28 @@ def add_expense(request):
 
     if not amount:
       messages.error(request, 'Please enter the amount!')
-      return render(request, 'expenses/add_expense.html', context)
+      return render(request, 'incomes/add_income.html', context)
 
     if not description:
       messages.error(request, 'Please enter the description!')
-      return render(request, 'expenses/add_expense.html', context)
+      return render(request, 'incomes/add_income.html', context)
 
     # create a new expense, use create
-    Expense.objects.create(owner=request.user, amount=amount, date=date,
+    Income.objects.create(owner=request.user, amount=amount, date=date,
                            description=description, category= category)
-    messages.success(request, 'Expense saved successfully!')
-    return redirect('expenses')
+    messages.success(request, 'Income saved successfully!')
+    return redirect('incomes')
 
-def edit_expense(request, id):
-  expense = Expense.objects.get(pk=id)
+def edit_income(request, id):
+  income = Income.objects.get(pk=id)
   categories = Category.objects.all()
   context = {
-    'expense': expense,
-    'values': expense,
+    'income': income,
+    'values': income,
     'categories': categories,
   }
   if request.method == 'GET':
-    return render(request, 'expenses/edit_expense.html', context)
+    return render(request, 'incomes/edit_income.html', context)
 
   if request.method == 'POST':
     amount = request.POST['amount']
@@ -88,26 +88,26 @@ def edit_expense(request, id):
 
     if not amount:
       messages.error(request, 'Please enter the amount!')
-      return render(request, 'expenses/edit_expense.html', context)
+      return render(request, 'incomes/edit_income.html', context)
 
     if not description:
       messages.error(request, 'Please enter the description!')
-      return render(request, 'expenses/edit_expense.html', context)
+      return render(request, 'incomes/edit_income.html', context)
 
-    # update the expense info, use save
-    expense.owner = request.user
-    expense.amount = amount
-    expense.date = date
-    expense.category = category
-    expense.description = description
+    # update the income info, use save
+    income.owner = request.user
+    income.amount = amount
+    income.date = date
+    income.category = category
+    income.description = description
 
-    expense.save()
+    income.save()
 
-    messages.success(request, 'Expense updated successfully!')
-    return redirect('expenses')
+    messages.success(request, 'Income updated successfully!')
+    return redirect('incomes')
 
-def delete_expense(request, id):
-  expense = Expense.objects.get(pk=id)
-  expense.delete()
-  messages.success(request, 'Expense deleted successfully!')
-  return redirect('expenses')
+def delete_income(request, id):
+  income = Income.objects.get(pk=id)
+  income.delete()
+  messages.success(request, 'Income deleted successfully!')
+  return redirect('incomes')
